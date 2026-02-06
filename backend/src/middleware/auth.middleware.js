18ced3d70d95cceb156034351e76002d3dd7e8ac
jihun <jxxparker@gmail.com>
2026-02-05 21:52:19 -0600

    pushign 1:12

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -9,5 +9,20 @@ export const protectRoute = async (req, res, next) => {
         .status(401)
         .json({ message: "Unauthorized - No Token Provided" });
     }
-  } catch (error) {}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - invalid token" });
+    }
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("error in protectroute middleware", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
