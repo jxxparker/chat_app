f2aac29c535bcfbc135fbd89fbac7d6a9ad7ee6b
jihun <jxxparker@gmail.com>
2026-02-07 22:39:01 -0600

    pushing up til 1:18

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
@@ -12,6 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
